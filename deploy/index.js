7daa334c2bf39deca11fbcc064d9eed1df0f2610
khai96_ <hvksmr1996@gmail.com>
2016-10-06 15:41:08 +0700

    Store first

--- deploy/index.js
+++ deploy/index.js
@@ -2,8 +2,14 @@
 
 const {
   env: {
-    GITHUB_RELEASE_OAUTH
-  }
+    GITHUB_RELEASE_OAUTH,
+    GIT_REPO_TAG,
+    GIT_REPO_OWNER,
+    GIT_REPO_NAME
+  },
+  stdout,
+  stderr,
+  exit
 } = require('process')
 
 const GitHubAPIs = require('github')
@@ -14,3 +20,35 @@ github.authenticate({
   type: 'token',
   token: GITHUB_RELEASE_OAUTH
 })
+
+const {repos} = github
+
+new Promise(
+  (resolve, reject) => {
+    const user = GIT_REPO_OWNER
+    const repo = GIT_REPO_NAME
+    const tag_name = GIT_REPO_TAG
+    const name = GIT_REPO_TAG
+    repos.editRelease({
+      user, repo, tag_name, name
+    })
+      .then(
+        () => {
+          repos.uploadAsset()
+        }
+      )
+      .catch(reject)
+  }
+)
+  .then(
+    () => {
+      stdout.write('Deploying succeed.\n')
+      exit(0)
+    }
+  )
+  .catch (
+    () => {
+      stderr.write('Deploying failed.\n')
+      exit(1)
+    }
+  )
