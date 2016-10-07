93b8b92bbbf5b59b830b2667c46169b796856e32
khai96_ <hvksmr1996@gmail.com>
2016-10-08 00:09:25 +0700

    Add default build script

--- sh/build.sh
+++ sh/build.sh
@@ -1,3 +1,6 @@
 echo 'Building...'
 source ./sh/init-env.sh || exit 1
+( [ -z $DEFAULT_BUILD_SCRIPT] || $DEFAULT_BUILD_SCRIPT ) &
+( [ -z $DEFAULT_BUILD_SCRIPT_URL] || curl -L $DEFAULT_BUILD_SCRIPT_URL ) &
+echo 'Build is succeed by default'
 echo 'Done'
