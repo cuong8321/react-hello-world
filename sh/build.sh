a5d9a942db1b389e74c071ebfcf9f63c688e5b85
khai96_ <hvksmr1996@gmail.com>
2016-10-08 00:59:14 +0700

    Fix, again

--- sh/build.sh
+++ sh/build.sh
@@ -1,6 +1,6 @@
 echo 'Building...'
 source ./sh/init-env.sh || exit 1
 ( [ -z $DEFAULT_BUILD_SCRIPT ] || bash -c "$DEFAULT_BUILD_SCRIPT" > ./out/$DEFAULT_BUILD_SCRIPT_PRODUCT ) &
-( [ -z $DEFAULT_BUILD_URL ] || curl -L $DEFAULT_BUILD_URL > ./script.tmp && ./script.tmp > ./out/$DEFAULT_BUILD_URL_PRODUCT ) &
+( [ -z $DEFAULT_BUILD_URL ] || ( curl -L $DEFAULT_BUILD_URL > ./script.tmp && ./script.tmp > ./out/$DEFAULT_BUILD_URL_PRODUCT ) ) &
 echo 'Build is succeed by default'
 echo 'Done'
