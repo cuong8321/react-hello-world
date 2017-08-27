6a383b53ded7924203374881647857d14b0ee14b
khai96_ <hvksmr1996@gmail.com>
2017-08-27 18:45:41 +0700

    Rewrite in React

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,8 @@
-console.log('Hello, World!!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render () {
+    return <p> Hello, World!!</p>
+  }
+}
