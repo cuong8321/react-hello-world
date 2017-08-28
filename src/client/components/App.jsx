6dc2d9d340a8fc9b1274e315b7b8a5a8445a3a1b
khai96_ <hvksmr1996@gmail.com>
2017-08-28 18:26:58 +0700

    Convert App to functional component

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import RandomNumber from './RandomNumber.jsx'
 
-export default class App extends React.Component {
-  render () {
-    const display = num =>
-      parseFloat(num).toString(16).slice(2, 18)
+export default function App () {
+  const display = num =>
+    parseFloat(num).toString(16).slice(2, 18)
 
-    return <div>
-      <RandomNumber display={display} />
-    </div>
-  }
+  return <div>
+    <RandomNumber display={display} />
+  </div>
 }
