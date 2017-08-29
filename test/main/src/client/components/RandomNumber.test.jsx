83315360b58457613e9b4529dbacbe09dba1c55b
khai96_ <hvksmr1996@gmail.com>
2017-08-29 15:09:05 +0700

    Add test cases

--- test/main/src/client/components/RandomNumber.test.jsx
+++ test/main/src/client/components/RandomNumber.test.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import RandomNumber from '../../subject-components/RandomNumber.jsx'
 
-test('Snapshot: components/RandomNumber.jsx', () => {
+describe('Snapshot: components/RandomNumber.jsx', () => {
   const init = () => 0.123456789
-  const tree = renderer.create(<RandomNumber init={init} />).toJSON()
-  expect(tree).toMatchSnapshot()
+
+  test('with default display function', () => {
+    const tree = renderer.create(<RandomNumber init={init} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('with specified display function', () => {
+    ; [
+      value => JSON.stringify({value}),
+      value => Number(value).toString(16).slice(2, 16),
+      value => Number(value).toString(36).slice(2, 13),
+      value => value * value,
+      value => [0, 1, 2, 3].map(x => value * x).join(', ')
+    ]
+      .map(display => (<RandomNumber init={init} display={display} />))
+      .map(element => renderer.create(element))
+      .map(instance => instance.toJSON())
+      .forEach(tree => expect(tree).toMatchSnapshot())
+  })
 })
