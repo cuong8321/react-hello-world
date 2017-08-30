d6cacf3cdf4aa8c8bec9b79e779a57628d8c6b81
khai96_ <hvksmr1996@gmail.com>
2017-08-30 10:15:52 +0700

    Material UI

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import AppBar from 'material-ui/AppBar'
+import DefaultMuiTheme from './DefaultMuiTheme.jsx'
 import RandomNumber from './RandomNumber.jsx'
 
 export default function App ({init}) {
@@ -6,6 +8,9 @@ export default function App ({init}) {
     parseFloat(num).toString(16).slice(2, 18)
 
   return <div>
+    <DefaultMuiTheme>
+      <AppBar title='Hello, World!!' />
+    </DefaultMuiTheme>
     <RandomNumber display={display} init={init} />
   </div>
 }
