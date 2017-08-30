f4fb0c1e3358994d2bcf5e4c86d85c0131ac4140
khai96_ <hvksmr1996@gmail.com>
2017-08-30 13:31:45 +0700

    Restructure webpack/module/rules

--- webpack/module/rules/index.js
+++ webpack/module/rules/index.js
@@ -1,21 +1,18 @@
 'use strict'
 const path = require('path')
-const {readFileSync} = require('fs')
+const {readFileSync, readdirSync} = require('fs')
 const yaml = require('js-yaml')
-const {js: jsregex, css: cssregex, exclude} = require('../../lib/regexes')
+const regexes = require('../../lib/regexes')
+const {exclude} = regexes
 
 const loadYamlFile = filename =>
-  yaml.safeLoad(readFileSync(path.resolve(__dirname, filename), 'utf8'))
+  yaml.safeLoad(readFileSync(filename, 'utf8'))
 
-module.exports = [
-  {
-    test: jsregex,
-    use: loadYamlFile('use-javascript.yaml'),
-    exclude
-  },
-  {
-    test: cssregex,
-    use: loadYamlFile('use-style.yaml'),
-    exclude
-  }
-]
+const dataUse = path.resolve(__dirname, 'data/use')
+
+const rules = readdirSync(dataUse)
+  .map(item => path.resolve(dataUse, item))
+  .map(file => [path.parse(file).name, loadYamlFile(file)])
+  .map(([name, use]) => ({test: regexes[name], use, exclude}))
+
+module.exports = rules
