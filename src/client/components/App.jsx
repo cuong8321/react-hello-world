0e5257963282e4ce9b1411da576033912078ec2e
khai96_ <hvksmr1996@gmail.com>
2017-08-30 17:46:43 +0700

    Display uppercase characters

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -5,7 +5,7 @@ import RandomNumber from './RandomNumber.jsx'
 
 export default function App ({init}) {
   const display = num =>
-    parseFloat(num).toString(16).slice(2, 18)
+    parseFloat(num).toString(16).slice(2, 18).toUpperCase()
 
   return <DefaultMuiTheme><div>
     <AppBar title='Hello, World!!' />
