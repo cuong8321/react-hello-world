e2ba10e4a845ab9425e2157232f6295696227e4e
Cuong <cuong8321@gmail.com>
2017-08-30 20:28:54 +0700

    Tạo component UpperCaseForm và thêm vào giao diện chính

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import DefaultMuiTheme from './DefaultMuiTheme.jsx'
 import RandomNumber from './RandomNumber.jsx'
+import UpperCaseForm from './UpperCaseForm.jsx'
 
 export default function App ({init}) {
   const display = num =>
     parseFloat(num).toString(16).slice(2, 18).toUpperCase()
 
   return <DefaultMuiTheme><div>
-    <AppBar title='Hello, World!!' />
+    <AppBar title='Hello, World!!' showMenuIconButton={false} />
     <RandomNumber display={display} init={init} />
+    <UpperCaseForm />
   </div></DefaultMuiTheme>
 }
---
+++ src/client/components/UpperCaseForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton'
+import Paper from 'material-ui/Paper'
+
+export default class UpperCaseForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+  render () {
+    const onChange = (event, text) => this.setState({text})
+    const clear = () => this.setState({text: ''})
+
+    return <Paper zDepth={1}><div>
+      <div>
+        <TextField hintText='Your Text Here...' onChange={onChange} value={this.state.text} />
+        <FlatButton secondary label='Clear' onClick={clear} />
+        <Paper zDepth={0}>
+          <label htmlFor='output'>Upper Case: </label>
+          <output id='output'>{this.state.text.toUpperCase() || <i>(Empty)</i>}</output>
+        </Paper>
+      </div>
+      <div>
+        <Paper zDepth={0} />
+      </div>
+    </div></Paper>
+  }
+}
