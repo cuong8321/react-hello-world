540a07ff4d0ca462cf17da70de17e5b8d7696162
khai96_ <hvksmr1996@gmail.com>
2017-08-30 13:15:49 +0700

    Add style loader

--- webpack/lib/regexes.js
+++ webpack/lib/regexes.js
@@ -1,4 +1,5 @@
 module.exports = {
   js: /\.(js|jsx|es[0-9]?x?)$/,
+  css: /\.css$/,
   exclude: /node_modules|bower_components/
 }
--- webpack/module/rules/index.js
+++ webpack/module/rules/index.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const {readFileSync} = require('fs')
 const yaml = require('js-yaml')
-const {js: jsregex, exclude} = require('../../lib/regexes')
+const {js: jsregex, css: cssregex, exclude} = require('../../lib/regexes')
 
 const loadYamlFile = filename =>
   yaml.safeLoad(readFileSync(path.resolve(__dirname, filename), 'utf8'))
@@ -11,6 +11,11 @@ module.exports = [
   {
     test: jsregex,
     use: loadYamlFile('use-javascript.yaml'),
+    exclude
+  },
+  {
+    test: cssregex,
+    use: loadYamlFile('use-style.yaml'),
     exclude
   }
 ]
