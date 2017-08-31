0ec6a40b5d36c88792c4d48ee222a590f00cbb0b
Cuong <cuong8321@gmail.com>
2017-08-31 20:16:18 +0700

    Fix standard

--- test/src/components/UpperCaseForm.test.jsx
+++ test/src/components/UpperCaseForm.test.jsx
@@ -2,7 +2,6 @@ import './__lib__/material-ui-mocks.jsx'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import UpperCaseForm from '../../../src/components/UpperCaseForm.jsx'
-import DefaultMuiTheme from '../../../src/components/DefaultMuiTheme.jsx'
 
 test('Snapshot: components/UpperCaseForm.jsx', () => {
   const tree = renderer.create(<UpperCaseForm />).toJSON()
